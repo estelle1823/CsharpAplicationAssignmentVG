651178553b0c07d070050753e71c417210de0352
estelle <estelle.sossou@yh.nackademin.se>
2025-10-18 20:59:41 +0200

    All Models Created.

---
+++ Infrastructure/Models/ProductManufacturer.cs
@@ -0,0 +1,7 @@
+﻿namespace Infrastructure.Models;
+
+public class ProductManufacturer
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Name { get; set; } = null!;
+}
