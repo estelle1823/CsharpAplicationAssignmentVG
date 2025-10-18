0dd9b9e9d69fe3390b95d6cf93d350d33f5e4edf
estelle <estelle.sossou@yh.nackademin.se>
2025-10-18 21:06:07 +0200

    Interface for ProductService created.

---
+++ Infrastructure/Interfaces/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Models;
+namespace Infrastructure.Interfaces;
+
+public interface IProductService
+{
+    bool CreateProduct(ProductCreateRequest product);
+
+    IEnumerable<Product> GetAllProducts();
+    Product GetProductById(Guid id);
+    Product GetProductByArticleNumber(string articleNumber);
+    Product GetProductByTitle(string productTitle);
+    Product GetProductByUnitPrice(decimal unitPrice);
+    Product SaveJsonContentToFile();
+    bool AddProductsToList(ProductCreateRequest product);
+
+    bool UpdateProduct(Guid id, ProductUpdateRequest product);
+    bool DeleteProduct(Guid productId);
+
+}
