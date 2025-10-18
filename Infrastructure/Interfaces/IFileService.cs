037ce8f76a448922eba62ab83a5a0863694f9f7d
estelle <estelle.sossou@yh.nackademin.se>
2025-10-18 21:04:15 +0200

    Interface for FileService created.

---
+++ Infrastructure/Interfaces/IFileService.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Interfaces;
+
+public interface IFileService
+{
+    bool SaveJsonContentToFile(string jsonContent, string fileName);
+
+    string GetJsonContentFromFile(string filePath);
+
+}
